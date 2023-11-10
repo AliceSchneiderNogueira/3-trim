2de0a2133e3e25d341bef6e80b3d58305164b040
AliceSchneiderNogueira <137070382+AliceSchneiderNogueira@users.noreply.github.com>
2023-11-10 19:57:32 -0300

    Add files via upload

---
+++ 3-trim-main/main.js
@@ -0,0 +1,36 @@
+function tocaSom (selectorAudio) {
+    const elememnto = document.querySelector(selectorAudio);
+   
+    if (elememento && elemento.localName === 'audio') {
+        elemento.play();
+
+    }
+    else {
+
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+
+const listaDeTeclas = document.querySelectorAll (".tecla"); 
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++)
+{
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = '#som_${instrumento}';
+
+  tecla.onclick = function ();
+    tocaSom(idAudio);
+}
+
+tecla.onkeydown = function (evento) {
+    if (evento.code === 'Enter' || evento.code === 'Space') {
+        tecla.classList.add('ativa');
+    }
+}
+
+
+
